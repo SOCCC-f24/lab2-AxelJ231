df95526e329b04416527f452eac8a0000b7189ba
AxelJ231 <adj16@sunyorange.edu>
2024-10-23 15:49:52 -0400

    Completed Decrypt Function

--- src/c_cipher.py
+++ src/c_cipher.py
@@ -56,24 +56,24 @@ def encrypt(email="abc012"):
 
 def decrypt(email="def345"):
     """
-    TODO: What is the objective? 
+    TODO: The objective is to take an encrypted email and decrypt it. 
 
     Args:
-        TODO: what arguments and data types are expected? (i.e., email)
+        TODO: The input should be an encrypted email. This email will be in the form of a string.
 
     Returns:
-        TODO: what varibale and data types are being returned?   
+        TODO: The returned variable will be retVal. It's data type is a string.   
     """
     # input validation
-    output = "" 
+     output = "" 
     len_flag = len(email) != 6
     # TODO: fix line below and, implement functionality rather than literals
     # keep all updates in the anum_flag (bool) variable
     # i.e., 
     #     A = email[:3] (check first half)
     #     B = email[3:] (check second half)
-    #     enum_flag = A or B
-    anum_flag = email[:3] != 'def' or email[3:] != '345' 
+    #     anum_flag = A or B
+    anum_flag = not((email[:3]).isalpha()) or not((email[3:]).isdecimal()) 
 
     if len_flag:                         # NOTE: here we provide input validation on length
         output = "Length check failed\n"
@@ -84,14 +84,26 @@ def decrypt(email="def345"):
         output = "alpha num check failed\n"
         output += "Email must have 3 letters followed by 3 digits."
         logging.info(output)
-        return output   
+        return output     
+        
+    # TODO: fix line below, process our string into a list
+    email_lst = list(email)
+        
+    # TODO: complete line(s) below, convert EACH new element into a string
+    new_ascii = []
+    for elem in email_lst: # NOTE: here we extract and update element at 0 
+        new_ascii.append(ord(elem) - 3)
+        
+    for index, elem in enumerate(new_ascii):  # NOTE: here we convert our ASCII into string
+        email_lst[index] = chr(elem)  
+        
+    # TODO: fix line below, convert list into a string
+    email_str = "".join(email_lst)
 
-    # TODO: apply the encrypt pseudocode but shift down 3
-    
     # keep all updates in the retVal (str) variablei
     # i.e.,
     #    email_str = " some string updates here "
     #    email_1 = email_str.strip()
     #    retVal = email_1
-    retVal = "aef345"
-    return retVal
+    retVal = email_str
+    return retVal 
